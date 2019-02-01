0d4e63875c56ef6de62b9e4a0104fe76f229499c
gmfc <gabriel.mfcorreia@gmail.com>
2019-02-01 11:20:29 -0200

    here we go

---
+++ indexOLD.ts
@@ -0,0 +1,33 @@
+import * as express from 'express'
+import { Server, RedisPresence } from 'colyseus'
+import { createServer } from 'http'
+import * as path from 'path'
+import { monitor } from '@colyseus/monitor'
+// import { MapController } from './server/rooms'
+
+const port = Number(process.env.PORT || 2567)
+const app = express()
+
+// Attach WebSocket Server on HTTP Server.
+const gameServer = new Server({
+  server: createServer(app),
+  verifyClient: (info, next) => {
+    next(true)
+  },
+  presence: new RedisPresence()
+})
+
+// gameServer.register('map', MapController, {
+// map: 'test'
+// })
+
+app.use('/', express.static(path.join(__dirname, 'static')))
+
+app.use('/monitor', monitor(gameServer))
+
+gameServer.onShutdown(() => {
+  console.log(`game server is going down.`)
+})
+
+gameServer.listen(port)
+console.log(`Listening on http://localhost:${port}`)
